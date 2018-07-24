e9291d23177f1c6c9cd9de522be0e0969faa51d5
tharle <tharlejc@gmail.com>
2018-07-24 12:51:08 -0400

    Add. function for detect size of window

--- src/app/contactmanager/components/sidenav/sidenav.component.ts
+++ src/app/contactmanager/components/sidenav/sidenav.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+
+const SMALL_WIDTH_BREAKPOINT = 720;//IN PX
 
 @Component({
   selector: 'app-sidenav',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
-
+  // private mediaMatcher: MediaQueryList = matchMedia(`(min-width): ${SMALL_WIDTH_BREAKPOINT}px`);
+  public innerWidth: any;
+  
   constructor() { }
 
   ngOnInit() {
+      this.innerWidth = window.innerWidth;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+  }
+
+
+  isScreenSmall(): boolean{
+    // console.log("Media matches: "+this.mediaMatcher.matches);
+    // return this.mediaMatcher.matches;
+    return this.innerWidth <= SMALL_WIDTH_BREAKPOINT;
   }
 
 }
